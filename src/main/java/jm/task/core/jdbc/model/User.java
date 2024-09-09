758655b195d97064b6e60c1c3016b66aa6c937eb
DVODeveloper <vyurkovd@yandex.ru>
2024-09-09 17:35:40 +0300

    exercise 1.1.4 - version 2

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -14,7 +14,7 @@ public class User {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "lastName")
+    @Column(name = "last_Name")
     private String lastName;
 
     @Column(name = "age")
